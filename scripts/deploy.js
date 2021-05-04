38b53eef3fedc19efa7353c97bd9a3cf00e886bb
Philipp-Florens Lehwalder <philipp@perun.network>
2021-05-05 00:44:23 +0200

    Now writes addresses of deployed contracts in JSON file
    Signed-off-by: Philipp-Florens Lehwalder <philipp@perun.network>

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,18 +11,32 @@ async function main() {
     const AssetHolderERC20Factory = await ethers.getContractFactory("AssetHolderERC20");
     const PerunTokenFactory = await ethers.getContractFactory("PerunToken");
 
-    
+    let deployedContracts = {};
+
     const channel = await ChannelFactory.deploy(); 
     console.log("Channel deployed to address:", channel.address);
+    deployedContracts.channel = channel.address;
+
     const adjudicator = await AdjudicatorFactory.deploy();
     console.log("Adjudicator deployed to address:", adjudicator.address);
+    deployedContracts.adjudicator = adjudicator.address;
 
     const assetholderETH = await AssetHolderETHFactory.deploy(adjudicator.address);
     console.log("AssetHolderETH deployed to address:", assetholderETH.address);
+    deployedContracts.assetholderETH = assetholderETH.address;
+
     const perunToken = await PerunTokenFactory.deploy(accAddresses, "1000000000000000000000");
     console.log("PerunToken deployed to address:", perunToken.address);
+    deployedContracts.perunToken = perunToken.address;
+
     const assetholderERC20 = await AssetHolderERC20Factory.deploy(adjudicator.address, perunToken.address);
     console.log("AssetHolderERC20 deployed to address:", assetholderERC20.address);
+    deployedContracts.assetholderERC20 = assetholderERC20.address;
+
+
+    const fs = require('fs');
+    let deployedContractsJSON = JSON.stringify(deployedContracts);
+    fs.writeFileSync('deployed-contracts.json', deployedContractsJSON);
 }
 
 main()
