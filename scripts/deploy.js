c6f31d5b167a9d60399e71b5a6486e6a412b17eb
Matthias Geihs <matthias@perun.network>
2021-05-05 16:21:58 +0200

    Increment nonce during deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,31 +5,35 @@ async function main() {
     accAddresses[0] = accounts[0].address; // Prefund Alice.
     accAddresses[1] = accounts[1].address; // Prefund Bob.
 
-    const ChannelFactory = await ethers.getContractFactory("Channel");
-    const AdjudicatorFactory = await ethers.getContractFactory("Adjudicator",);
-    const AssetHolderETHFactory = await ethers.getContractFactory("AssetHolderETH");
-    const AssetHolderERC20Factory = await ethers.getContractFactory("AssetHolderERC20");
-    const PerunTokenFactory = await ethers.getContractFactory("PerunToken");
+    const deployerAddress = (await getNamedAccounts()).deployer;
+    const deployer = await ethers.getSigner(deployerAddress);
+    let nonce = await deployer.getTransactionCount();
+
+    const ChannelFactory = await ethers.getContractFactory("Channel", deployer);
+    const AdjudicatorFactory = await ethers.getContractFactory("Adjudicator", deployer);
+    const AssetHolderETHFactory = await ethers.getContractFactory("AssetHolderETH", deployer);
+    const AssetHolderERC20Factory = await ethers.getContractFactory("AssetHolderERC20", deployer);
+    const PerunTokenFactory = await ethers.getContractFactory("PerunToken", deployer);
 
     let deployedContracts = {};
 
-    const channel = await ChannelFactory.deploy(); 
+    const channel = await ChannelFactory.deploy(overrides = {nonce: nonce++}); 
     console.log("Channel deployed to address:", channel.address);
     deployedContracts.channel = channel.address;
 
-    const adjudicator = await AdjudicatorFactory.deploy();
+    const adjudicator = await AdjudicatorFactory.deploy(overrides = {nonce: nonce++});
     console.log("Adjudicator deployed to address:", adjudicator.address);
     deployedContracts.adjudicator = adjudicator.address;
 
-    const assetholderETH = await AssetHolderETHFactory.deploy(adjudicator.address);
+    const assetholderETH = await AssetHolderETHFactory.deploy(adjudicator.address, overrides = {nonce: nonce++});
     console.log("AssetHolderETH deployed to address:", assetholderETH.address);
     deployedContracts.assetholderETH = assetholderETH.address;
 
-    const perunToken = await PerunTokenFactory.deploy(accAddresses, "1000000000000000000000");
+    const perunToken = await PerunTokenFactory.deploy(accAddresses, "1000000000000000000000", overrides = {nonce: nonce++});
     console.log("PerunToken deployed to address:", perunToken.address);
     deployedContracts.perunToken = perunToken.address;
 
-    const assetholderERC20 = await AssetHolderERC20Factory.deploy(adjudicator.address, perunToken.address);
+    const assetholderERC20 = await AssetHolderERC20Factory.deploy(adjudicator.address, perunToken.address, overrides = {nonce: nonce++});
     console.log("AssetHolderERC20 deployed to address:", assetholderERC20.address);
     deployedContracts.assetholderERC20 = assetholderERC20.address;
 
