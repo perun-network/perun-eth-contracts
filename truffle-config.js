800861abfab8b9e2a9d6f14e9171cbdd098c801c
Matthias Geihs <matthias@perun.network>
2021-05-11 12:31:49 +0200

    :construction_worker: Etherscan verify & publish
    Signed-off-by: Matthias Geihs <matthias@perun.network>

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,28 @@
+var PrivateKeyProvider = require("truffle-privatekey-provider");
+// For deployment to a public network, enter your credentials here.
+var GOERLI_NETWORK_URL = "";
+var DEPLOYER_SECRET_KEY = "";
+var ETHERSCAN_API_KEY = "";
+
 module.exports = {
-    plugins: ["truffle-security", "solidity-coverage"],
+    api_keys: {
+        etherscan: ETHERSCAN_API_KEY
+    },
+
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "*",
+        },
+        goerli: {
+            provider: () => new PrivateKeyProvider(
+                DEPLOYER_SECRET_KEY,
+                GOERLI_NETWORK_URL,
+            ),
+            network_id: 5,
+        },
+    },
 
     mocha: {
         reporter: 'eth-gas-reporter',
@@ -12,7 +35,7 @@ module.exports = {
 
     compilers: {
         solc: {
-            version: "^0.7.0",
+            version: "0.7.6",
             settings: {
                 optimizer: {
                     enabled: true,
@@ -20,5 +43,7 @@ module.exports = {
                 },
             },
         }
-    }
+    },
+
+    plugins: ["solidity-coverage", "truffle-plugin-verify"],
 }
